8685e2c9f81dfd599372f13e318b022ff7dc7d98
radsi <78619513+radsi@users.noreply.github.com>
2022-08-11 12:48:19 +0200

    Update Skyreader API.AssemblyInfo.cs

--- obj/Debug/netcoreapp3.1/Skyreader API.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/Skyreader API.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Skyreader API")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SkylanderRealPortal API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Skyreader API")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Skyreader API")]
+[assembly: System.Reflection.AssemblyProductAttribute("SkylanderRealPortal API")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SkylanderRealPortal API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
